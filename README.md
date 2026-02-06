d00d18532ce02ea0f6978b7975a408b18d883c72
Christian Driscoll <59884498+solinym@users.noreply.github.com>
2026-02-06 12:44:27 -0500

    1.1 Release

--- README.md
+++ README.md
@@ -1,41 +1,58 @@
-# PicoNandwich
+
+# PicoNandwich (v1.1 UPDATE)
 PicoNandwich is a dual nand solution for the Xbox 360 that uses a Raspberry Pi Pico for nand switching (and other features).
 
 ### Pinout/Wiring
 Both CE points must be pulled HIGH to 3.3v with a resistor inline. (Tested both 3.3k and 10k, both work fine.)
-|Pico|Console|
-|--|--|
-|GPIO 0|BUZZER +|
-|GPIO 1|ROL LED POINT|
-|GND|BUZZER -|
-|GPIO 2|SYNC POINT|
-|GPIO 3|NAND 1 CE|
-|GPIO 4|NAND 2 CE|
-|VSYS|5V or 3.3V Standby|
-|GND|Console GND|
+|Pico|Console|Info|
+|--|--|--|
+|GPIO 0|BUZZER +|Speaker/Buzzer Positive (optional)|
+|GPIO 1|ROL LED POINT|Goes to spare pin on RF board connector for LED (optional, but recommended)|
+|GND|BUZZER -|Speaker/Buzzer Negative (optional)|
+|GPIO 2|SYNC POINT|Goes to sync pin on RF board connector|
+|GPIO 3|NAND 1 CE|Nand 1 Chip Enable|
+|GPIO 4|NAND 2 CE|Nand 2 Chip Enable|
+|GPIO 28 -> GPIO 17|UART TX|Bridge GP28 and 17, used for UART and Power Sensing (optional, but recommended)|
+|VSYS|5V or 3.3V Standby|Power to the pico even when console is off|
+|GND|Console GND|obvious|
 
 ### How To Use
 
  - Hold the sync button briefly, LED/Buzzer will indicate which NAND is selected by lighting up or beeping one, two, three or four times depending on the setup. 
  - Hold the sync button for around 8 seconds to put the Pico into BOOTSEL/bootloader mode. Pico will flash 3 times with a long beep. (If you don't have access to the Pico's physical button, do NOT turn off the console or unplug the cable since this will wipe the firmware and you will have no way to write the firmware back to the Pico without opening your console.)
- 
 
 
 ### Quality of Life Features
- - NAND Save States - Remembers which nand was selected after power loss.
- - Serial Console - For those technical nerds.
- - LED - While standard on dual nand consoles, ill mention it anyways. Wire an ROL LED to the pico.
- - Buzzer/Speaker - Wire up a buzzer/speaker for audio feedback. (cause why not?)
- - BOOTSEL Mode - Reflash the Pico's firmware while installed in the console (USB must be plugged in)
+ - **Persistent Config** - Saves all settings and active nand between power cycles.
+ - **Serial Console** - Configure your PicoNandwich and get information.
+ - **UART Passthrough** - View your console's UART/Debug output.
+ - **Power Sensing** - Detect if the console is powered on and prevent nand switching. (***Disabled By Default***)
+ - **PS Calibrate** -  Auto Calibrates power sensing threshold. (***Needed, values can vary from console to console and wiring***)
+  - **BOOTSEL Mode** - Reflash the Pico's firmware while installed in the console (***USB must be plugged in***)
+ - **LED** - While standard on dual nand consoles, ill mention it anyways. Wire an ROL LED to the pico.
+ - **Buzzer/Speaker** - Wire up a buzzer/speaker for audio feedback. (cause why not?)
 
 
 ### Serial Features
-This firmware contains some Serial features as well. You can check the status of the pico/console and send commands back. It uses a 115200 Baud Rate.
+This firmware contains Serial features as well. You will use this to configure power sensing. You can also use it to send other various commands to the pico, view pico/nand status and view your console's UART output.
 
-![Console](https://github.com/solinym/PicoNandwich/blob/main/Arduino_IDE_ldOfcprlcx.png?raw=true)
+![Console](image.png)
+![Console](video.mp4)
+
+**Calibrating Power Sense**
+
+ 1. Plug USB into PC and run your serial software of choice; something that can send commands.
+ 2. Select correct COM port and set baud rate to 115200.
+ 3. Run "help" to see all available commands, we are looking for `ps_toggle`; run that command.
+ 4. To calibrate automatically, power your console on and then run the `ps_calibrate` command.
+ 5. To test, power off your console and try to switch nands (or check status), it should work. Now turn your console on, nand switching should no longer work while console is turned on and status command should show console as on.
+
+***Note**: if your console doesnt boot straight away, check your wiring and then disable PS and calibrate it manually using `ps_monitor` and `ps_set`. `ps_set` should be at just below the average number `ps_monitor` gives you.*
 
 ### ***Some Disclaimers***
-Power sensing is NOT yet implemented, so do this at your own risk. I am not responsible for corrupted NANDs OR banned consoles. **DO NOT HOTSWAP YOUR NAND UNLESS YOU KNOW WHAT YOU'RE DOING.**
+I am NOT responsible for any bricked consoles or corrupted NANDs or console bans from doing this install! Power sensing works but is imperfect, so please do **NOT** hotswap NANDs if you don't know what you are doing.
 
 I **DO NOT** recommend powering the console while connected to a PC under normal circumstances. This risks backfeeding your console from the PC's USB port. Typically the Pico has protection against this, but not when voltages over USB and the VSYS pin are drastically different. 
 If you do this, make sure to have the proper protections (like a diode) in place. (Do this if you're using serial features too.)
+
+^^ You can do this without added protection, this is what i have done myself. But I am not responsible for any damage that may or may not be caused to your console/pico.
--- picoDualNand.ino
+++ picoDualNand.ino
@@ -1,6 +1,20 @@
-#include "pico/bootrom.h"
-#include <EEPROM.h>
+/*
+ * PicoNandwich v1.1
+ * Dual-NAND Switching for Xbox 360 using Raspberry Pi Pico
+ * Created by: solinym
+ */
 
+#include "pico/bootrom.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include <EEPROM.h>
+#include <Wire.h>
+
+#define UART_ID uart0
+#define BAUD_RATE 115200
+#define XBOX_RX_PIN 17
+
+// Hardware Pin Definitions
 const int CE_NAND_A   = 3;
 const int CE_NAND_B   = 4;
 const int SYNC_BUTTON = 2;
@@ -8,6 +22,18 @@ const int LED_PIN     = LED_BUILTIN;
 const int LED_EXT     = 1;
 const int BEEP_PIN    = 0;
 
+// Persistent Configuration Structure
+struct Config {
+  uint32_t magic;
+  uint8_t activeNand;
+  bool uartEnabled;
+  bool powerSenseOn;
+  float psAverage;
+  float version;
+};
+
+Config sysSettings;
+const uint32_t SETTINGS_MAGIC = 0x534F4C49;
 bool nandASelected = true;
 bool lastButtonState = HIGH;
 const int EEPROM_SIZE = 256; 
@@ -15,39 +41,37 @@ const int ADDR_STATE  = 0;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("--- PicoNandwich v1.0 | By: solinym ---");
+  Serial.println("--- PicoNandwich v1.1 | By: solinym ---");
+
+  EEPROM.begin(EEPROM_SIZE);
+  loadConfig();
+
+  // UART Setup
+  Serial1.setRX(XBOX_RX_PIN);
+  Serial1.begin(115200);
+
   pinMode(CE_NAND_A, OUTPUT);
   pinMode(CE_NAND_B, OUTPUT);
   pinMode(SYNC_BUTTON, INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
   pinMode(BEEP_PIN, OUTPUT);
   pinMode(LED_EXT, OUTPUT);
-  EEPROM.begin(EEPROM_SIZE);
-  uint8_t savedState = EEPROM.read(ADDR_STATE);
-  
-  if (savedState == 0) { //NAND 1 Save State
-    nandASelected = true;
-    Serial.println("SAVE STATE: Loaded NAND 1");
-  } else if (savedState == 1) { //NAND 2 Save State
-    nandASelected = false;
-    Serial.println("SAVE STATE: Loaded NAND 2");
-  } else { //Default to NAND 1 if flash is blank.
-    Serial.println("SAVE STATE: Flash is blank, defaulting to NAND 1.");
-    nandASelected = true;
-    updateStorage(0);
-  }
+
+  // Initialize NAND state from flash
+  nandASelected = (sysSettings.activeNand == 0);
   applyNandLogic(false);
 }
 
+// Button timing constants
 const unsigned long HOLD_TIME = 750;
 unsigned long pressStart = 0;
 bool holding = false;
 const unsigned long BOOTSEL_HOLD = 8000;
 
-//Main Loop | Handles sync button logic
 void loop() {
   bool button = digitalRead(SYNC_BUTTON);
 
+  // Process USB Serial Commands
   if (Serial.available() > 0) {
     String command = Serial.readStringUntil('\n');
     command.trim(); 
@@ -56,41 +80,55 @@ void loop() {
     }
   }
 
+  // Process SMC UART logs
+  if (Serial1.available() && sysSettings.uartEnabled == true) {
+    while (Serial1.available()) {
+      char c = Serial1.read();
+      Serial.print(c); 
+    }
+  }
+
+  // Handle Button Press
   if (button == LOW && lastButtonState == HIGH) {
     pressStart = millis();
     holding = false;
   }
 
+  // Handle long-press for BOOTSEL
   if (button == LOW) {
     unsigned long held = millis() - pressStart;
-
     if (held > BOOTSEL_HOLD) {
-      Serial.println("SYNC_BTN: Entering BOOTSEL/flashing mode. REMEMBER: YOU CAN NOT RE-ENTER BOOTSEL IF PICO DOESNT HAVE PicoNandwich INSTALLED OR IF YOU CAN ACCESS THE PHYSICAL BUTTON ON THE PICO. DO NOT UNPLUG YOUR CONSOLE.");
+      Serial.println("[SYNC_BTN]: Entering BOOTSEL/flashing mode...");
       blinkLEDTime(3, 500);
       beepTime(1, 2000, 500);
       reset_usb_boot(0, 0);
     }
   }
 
+  // Handle short-press for NAND toggle
   if (button == LOW && !holding && millis() - pressStart > HOLD_TIME) {
     holding = true;
-    Serial.println("SYNC_BTN: Switching NAND");
+    Serial.println("[SYNC_BTN]: Switching NAND");
     toggleNand();
   }
 
   lastButtonState = button;
 }
 
-//Toggles between NAND 1/NAND 2 (will be deprecated with triple/quad nand release)
+// Logic to switch NAND banks
 void toggleNand() {
+  if(isConsoleOn() && sysSettings.powerSenseOn == true){
+    Serial.println("[NAND]: Switch denied. Please power off your console first.");
+    return;
+  }
   nandASelected = !nandASelected;
-  Serial.println(nandASelected ? "NAND: Switched to NAND 1" : "NAND: Switched to NAND 2");
+  Serial.println(nandASelected ? "[NAND]: Switched to NAND 1" : "[NAND]: Switched to NAND 2");
 
   applyNandLogic(true);
-  updateStorage(nandASelected ? 0 : 1);
+  saveConfig();
 }
 
-//Nand switching logic
+// Pin level logic for Chip Enable (CE)
 void applyNandLogic(bool notify) {
   if (nandASelected) {
     digitalWrite(CE_NAND_A, LOW);
@@ -109,21 +147,30 @@ void applyNandLogic(bool notify) {
   }
 }
 
-//Update save state in Pico flash
-void updateStorage(uint8_t val) {
-  // Only write if the value has changed.
-  if (EEPROM.read(ADDR_STATE) != val) {
-    EEPROM.write(ADDR_STATE, val);
-    if(EEPROM.commit()) {
-       Serial.println("SAVE STATE: NAND state saved to flash.");
-    } else {
-       Serial.println("SAVE STATE: ERROR saving NAND state to flash!");
-    }
+// Write settings to EEPROM
+void saveConfig() {
+  sysSettings.magic = SETTINGS_MAGIC;
+  sysSettings.activeNand = nandASelected ? 0 : 1;
+  EEPROM.put(ADDR_STATE, sysSettings);
+  EEPROM.commit();
+}
+
+// Load settings from EEPROM
+void loadConfig() {
+  EEPROM.get(ADDR_STATE, sysSettings);
+  if (sysSettings.magic != SETTINGS_MAGIC || sysSettings.version < 1.1) {
+    Serial.println("[SAVE STATE]: Flash is blank/old, initializing defaults.");
+    sysSettings.magic = SETTINGS_MAGIC;
+    sysSettings.activeNand = 0;
+    sysSettings.uartEnabled = false;
+    sysSettings.powerSenseOn = false;
+    sysSettings.psAverage = 350.0;
+    sysSettings.version = 1.1;
+    saveConfig();
   }
 }
 
-
-//Blink onboard/ROL LED (Number of blinks, Duration of blinks)
+// Feedback: LED Blinking
 void blinkLEDTime(int times, int duration){
   for(int i=0; i<times; i++){
     digitalWrite(LED_PIN, HIGH);
@@ -135,7 +182,7 @@ void blinkLEDTime(int times, int duration){
   }
 }
 
-//Play beeps through a speaker/buzzer (Number of beeps, Duration of beeps, Delay between beeps)
+// Feedback: Buzzer Beeps
 void beepTime(int times, int duration, int del){
   for(int i=0; i<times; i++){
     tone(BEEP_PIN, 2000, duration);
@@ -143,40 +190,89 @@ void beepTime(int times, int duration, int del){
   }
 }
 
-//Handle Serial Commands (lol this is so bad)
+// Detect if console is powered via ADC on GP28
+bool isConsoleOn() {
+  int readings = 0;
+  int count = 5; 
+  for(int i = 0; i < count; i++) {
+    readings += analogRead(28);
+    delay(1); 
+  }
+  float average = readings / (float)count;
+  return (average >= sysSettings.psAverage); 
+}
+
+// Serial Terminal Command Processor
 void handleStringCommands(String cmd) {
   if (cmd == "switch_nand") {
-    Serial.println("USB_DEBUG: Switching NAND...");
+    Serial.println("[USB_COM]: Switching NAND...");
     toggleNand();
   } 
   else if (cmd == "bootsel") {
-    Serial.println("USB_DEBUG: Resetting Pico to bootloader... (REMEMBER: YOU CAN NOT RE-ENTER BOOTSEL IF PICO DOESNT HAVE PicoNandwich INSTALLED OR IF YOU CAN ACCESS THE PHYSICAL BUTTON ON THE PICO. DO NOT UNPLUG YOUR CONSOLE.)");
+    Serial.println("[USB_COM]: Entering Bootloader...");
     delay(500);
     reset_usb_boot(0, 0);
   } 
   else if (cmd == "status") {
-    Serial.println("");
-    Serial.println("--- PicoNandwich Status ---");
-    Serial.println("Version: v1.0");
-    Serial.print("Active NAND: ");
-    Serial.println(nandASelected ? "1" : "2");
-    Serial.print("CE 1 STATUS: ");
-    Serial.println(digitalRead(CE_NAND_A) == LOW ? "LOW" : "HIGH");
-    Serial.print("CE 2 STATUS: ");
-    Serial.println(digitalRead(CE_NAND_B) == LOW ? "LOW" : "HIGH");
-    Serial.println("CONSOLE POWER STATUS: N/A (Not Implemented)");
+    Serial.println("\n--- PicoNandwich Status ---");
+    Serial.println("Version: v1.1");
+    Serial.print("Active NAND: "); Serial.println(nandASelected ? "1" : "2");
+    Serial.print("CE 1 STATUS: "); Serial.println(digitalRead(CE_NAND_A) == LOW ? "LOW" : "HIGH");
+    Serial.print("CE 2 STATUS: "); Serial.println(digitalRead(CE_NAND_B) == LOW ? "LOW" : "HIGH");
+    Serial.print("UART LOGGING: "); Serial.println(sysSettings.uartEnabled ? "ENABLED" : "DISABLED");
+    Serial.print("POWER SENSING: "); Serial.println(sysSettings.powerSenseOn ? "ENABLED" : "DISABLED");
+    Serial.print("PS THRESHOLD: "); Serial.println(sysSettings.psAverage);
+    Serial.print("CONSOLE POWER: "); Serial.println(isConsoleOn() ? "ON" : "OFF");
     Serial.println("");
   } 
   else if (cmd == "help") {
-    Serial.println("");
-    Serial.println("--- Available Commands ---");
-    Serial.println("switch_nand - Switches between available NANDs.");
-    Serial.println("status      - Shows active NAND and other useful information.");
-    Serial.println("bootsel     - Reboots Pico for firmware update.");
-    Serial.println("");
+    Serial.println("\n--- Available Commands ---");
+    Serial.println("switch_nand   - Toggle NAND banks");
+    Serial.println("uart_toggle   - Toggle SMC UART logging");
+    Serial.println("ps_toggle     - Toggle Power Sensing safety");
+    Serial.println("ps_set <val>  - Manual threshold set");
+    Serial.println("ps_calibrate  - Auto-tune threshold");
+    Serial.println("ps_monitor    - Live ADC monitoring");
+    Serial.println("status        - System status");
+    Serial.println("bootsel       - Update firmware\n");
   } 
+  else if (cmd == "uart_toggle"){
+    sysSettings.uartEnabled = !sysSettings.uartEnabled;
+    saveConfig();
+    Serial.println(sysSettings.uartEnabled ? "[USB_COM]: UART Enabled" : "[USB_COM]: UART Disabled");
+  }
+  else if (cmd == "ps_toggle"){
+    sysSettings.powerSenseOn = !sysSettings.powerSenseOn;
+    saveConfig();
+    Serial.println(sysSettings.powerSenseOn ? "[USB_COM]: Power Sensing Enabled" : "[USB_DEBUG]: Power Sensing Disabled");
+  }
+  else if (cmd.startsWith("ps_set ")){
+    sysSettings.psAverage = cmd.substring(7).toFloat();
+    saveConfig();
+    Serial.print("[USB_COM]: Power Sensing threshold set to: "); Serial.println(sysSettings.psAverage);
+  }
+  else if (cmd == "ps_calibrate"){
+    Serial.println("[USB_COM]: Calibrating... Ensure console is ON.");
+    long total = 0;
+    for(int i=0; i<50; i++) {
+        total += analogRead(28);
+        delay(10);
+    }
+    float avg = total / 50.0;
+    if(avg < 50) Serial.println("[USB_COM]: Voltage low. Check connections!");
+    sysSettings.psAverage = avg * 0.8; 
+    saveConfig();
+    Serial.print("[USB_COM]: Calibrated! New threshold: "); Serial.println(sysSettings.psAverage);
+  }
+  else if (cmd == "ps_monitor") {
+    Serial.println("[USB_COM]: Monitoring ADC (5 seconds)...");
+    for(int i=0; i<20; i++) {
+      Serial.print("ADC: "); Serial.println(analogRead(28));
+      delay(250);
+    }
+  }
   else {
-    Serial.print("USB_DEBUG: Unknown command: '");
+    Serial.print("[USB_COM]: Unknown command: '");
     Serial.print(cmd);
     Serial.println("' - Type 'help' for list.");
   }
